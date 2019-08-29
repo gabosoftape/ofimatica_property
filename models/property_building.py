659b9075338769ead38d8e15c4716b5199448c97
gabriel <gabosoft.ape@gmail.com>
2019-08-29 15:33:00 -0500

    update mascotas

--- models/property_building.py
+++ models/property_building.py
@@ -19,6 +19,7 @@ class PropertyBuilding(models.Model):
     categ_id = fields.Many2one('property.building.categ', string="Categoria")
     room_ids = fields.One2many('property.room', 'building_id', string="Lugares")
     vehiculos_ids = fields.One2many('property.vehicle', 'building_id', string="Vehiculos")
+    mascotas_ids = fields.One2many('property.pet', 'building_id',string="Mascotas")
     features_ids = fields.One2many('property.features', 'building_id', string="Features")
 
     administrator_id = fields.Many2one('res.partner', string="Propietario", domain=[('is_company', '=', False)])
