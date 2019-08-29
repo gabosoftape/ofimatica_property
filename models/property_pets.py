f54185320c4fa3d1efd88bce730a0b2dacb94a52
gabriel <gabosoft.ape@gmail.com>
2019-08-29 15:39:32 -0500

    NORMALIZE

--- models/property_pets.py
+++ models/property_pets.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models, _
 
 class PropertyPet (models.Model):
     _name = 'property.pet'
-
+    building_id = fields.Many2one('property.building', string='Inmueble', required=True)
     foto_placa = fields.Binary('Foto Placa')
     foto = fields.Binary('Foto Animal')
     tipo = fields.Char(string="Tipo")
