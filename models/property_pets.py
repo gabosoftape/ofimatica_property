00b595fa3dd61a6b8c247a6330044e9801650c48
gabriel <gabosoft.ape@gmail.com>
2019-09-02 12:02:26 -0500

    update changes of the pets and vehicles

--- models/property_pets.py
+++ models/property_pets.py
@@ -4,9 +4,11 @@ from odoo import api, fields, models, _
 class PropertyPet (models.Model):
     _name = 'property.pet'
     building_id = fields.Many2one('property.building', string='Inmueble', required=True)
-    foto_placa = fields.Binary('Foto Placa')
-    foto = fields.Binary('Foto Animal')
-    tipo = fields.Char(string="Tipo")
-    raza = fields.Char(string="Raza")
+    #foto_placa = fields.Binary('Foto Placa')
+    #foto = fields.Binary('Foto Animal')
+    #tipo = fields.Char(string="Tipo")
     nombre = fields.Char(string="Nombre")
+    raza = fields.Char(string="Raza")
     color = fields.Char(string="Color")
+    seña = fields.Char(string="Seña Particular")
+    placa = fields.Char(string="No. Placa")
--- models/property_vehicles.py
+++ models/property_vehicles.py
@@ -5,7 +5,9 @@ class PropertyVehicle(models.Model):
     _name = 'property.vehicle'
     _rec_name = 'placa'
     building_id = fields.Many2one('property.building', string='Inmueble', required=True)
-    foto = fields.Binary('Foto')
+    #foto = fields.Binary('Foto')
+    propietario = fields.Char('Propietario')
+    doc_propietario = fields.Char('Documento Propietario')
     tipo = fields.Selection([('moto','Moto'),('carro','Carro')],string="Tipo")
     marca = fields.Char(string="Marca")
     modelo = fields.Char(string="Modelo")
