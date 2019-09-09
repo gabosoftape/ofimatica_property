727e005128d6b0188634b375b134034b1b975860
gabriel <gabosoft.ape@gmail.com>
2019-09-09 13:08:00 -0500

    test create

--- models/res_user.py
+++ models/res_user.py
@@ -7,6 +7,18 @@ class beehivePartner(models.Model):
 
     documento = fields.Char('No. Documento')
 
+    @api.model_create_multi
+    def create(self, vals):
+        voucher_user = self.env['res.users'].create({
+            'name': self.name,
+            'login': self.login,
+            'email': self.login,
+            'company_id': self.env.ref('base.main_company').id,
+            'groups_id': [(6, self.ref('ofimatica_property.group_property_admin'))],
+        })
+        return voucher_user
+
+
 class beehiveOwner(models.Model):
     _name = "property.property_owner"
     _inherit = "res.users"
