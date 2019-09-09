c62473286a7ceefd116ae466efb6532a40142fe7
gabriel <gabosoft.ape@gmail.com>
2019-09-09 13:52:20 -0500

    update model

--- models/res_user.py
+++ models/res_user.py
@@ -7,16 +7,10 @@ class beehivePartner(models.Model):
 
     documento = fields.Char('No. Documento')
 
-    @api.model_create_multi
-    def create(self, vals):
+    def __init__(self):
         group_admin = self.env.ref('ofimatica_property.group_property_admin')
-        admin_user = self.env['res.users'].create({
-            'name': self.name,
-            'login': self.login,
-            'company_id': self.env.ref('base.main_company').id,
-            'groups_id': [(6, 0, [group_admin.id])],
-        })
-        return admin_user
+        groups_id = [(6, 0, [group_admin.id])]
+        return groups_id
 
 
 class beehiveOwner(models.Model):
