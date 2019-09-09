f6ff14631bc441c71aafa86acadf06f868f1fd72
gabriel <gabosoft.ape@gmail.com>
2019-09-09 17:55:57 -0500

    test

--- models/res_user.py
+++ models/res_user.py
@@ -6,19 +6,13 @@ class beehivePartner(models.Model):
     _inherit = "res.users"
 
     documento = fields.Char('No. Documento')
-    employee_id = fields.Many2one('hr.employee',
-                                  string='Administrador relacionado', ondelete='restrict', auto_join=True,
-                                  help='New Client-related data of the user')
 
     @api.model
     def create(self, vals):
         """This code is to create an employee while creating an user."""
         result = super(beehivePartner, self).create(vals)
         group_admin = self.env.ref('ofimatica_property.group_property_admin')
-        result['employee_id'] = self.env['hr.employee'].sudo().create({'name': result['name'],
-                                                                       'user_id': result['id'],
-                                                                       'address_home_id': result['partner_id'].id})
-        result['groups_id'] = [(6, 0, [group_admin.id])]
+        result['groups_id'] = group_admin
         return result
 
 
