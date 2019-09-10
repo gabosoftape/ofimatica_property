ce102d349edc18048b25e6c993f03aa429e64a84
gabriel <gabosoft.ape@gmail.com>
2019-09-10 13:00:07 -0500

    .

--- models/res_user.py
+++ models/res_user.py
@@ -7,15 +7,6 @@ class beehivePartner(models.Model):
 
     documento = fields.Char('No. Documento')
 
-    @api.model
-    def create(self, vals):
-        """This code is to create an employee while creating an user."""
-        result = super(beehivePartner, self).create(vals)
-        group_admin = self.env.ref('ofimatica_property.group_property_admin')
-        result['groups_id'] = group_admin
-        result['uid'] = self.id
-        return result
-
 
 class beehiveOwner(models.Model):
     _name = "property.property_owner"
