574b88c6c45772c853d974cf53c159efbab4cfdb
gabriel <gabosoft.ape@gmail.com>
2019-10-01 10:55:20 -0500

    remove inhetited res.users class into

--- models/res_user.py
+++ models/res_user.py
@@ -72,7 +72,6 @@ class beehiveOwner(models.Model):
 
 class beehiveLessee(models.Model):
     _name = "property.property_lessee"
-    _inherit = "res.users"
 
     documento = fields.Char('No. Documento')
     land_id = fields.Many2one('property.land', string="Conjunto asociado")
