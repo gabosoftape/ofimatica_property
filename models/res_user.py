675a3fe393eadfd4d251da50c4bf337366843692
gabriel <gabosoft.ape@gmail.com>
2019-10-08 16:01:05 -0500

    update to test

--- models/res_user.py
+++ models/res_user.py
@@ -34,7 +34,7 @@ class beehivePartner(models.Model):
                                                                        'user_id': new_user.id,
                                                                        'address_home_id': result['partner_id'].id,
                                                                        'company_id': self.env.ref('base.main_company').id})
-        user = self.env['res.users'].sudo().browse(new_user)
+        user = self.env['res.users'].sudo().search([('login', '=',vals['login'])])
         user.action_reset_password()
         return result
 
